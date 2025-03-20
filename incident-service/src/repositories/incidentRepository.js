700280de5a5d69d278091619913b0deadea8a945
jean-francoisB <antalissenart@gmail.com>
2025-03-20 16:31:57 +0100

    ajout des fichiers

--- incident-service/src/repositories/incidentRepository.js
+++ incident-service/src/repositories/incidentRepository.js
@@ -2,19 +2,17 @@ import Incident from "../models/Incident.js"
 import Team from "../models/Team.js"
 
 export const createIncident = async (data) => {
-    // Vérification si une équipe est dispo et on sélectionne la plus ancienne en attente
+    
     const availableTeam = await Team.findOneAndUpdate(
         {
             availability: true
-        }, // Le filtre de recherche
+        }, 
         {
             availability: false
-        },//Le champ que je veux mettre à jour, en l'occurence je change la disponibilité de l'équipe
+        },
         {
             sort: { updatedAt: 1 }
-        } // Exemple :Team A prend a cloturé un incident à 14h00 => Availability : true DONC updatedAt: 14h00
-        // Team B cloture un incident à 10h00 => Availability: true DONC updatedAt: 10h00
-        // En réalisant le sort, il me prendra l'équipe de 10h00
+        } 
     )
 
     if (!availableTeam) {
